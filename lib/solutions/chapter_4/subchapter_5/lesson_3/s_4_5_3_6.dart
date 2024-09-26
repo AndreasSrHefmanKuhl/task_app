70eac3f526f47c06586cac74d24e16e0a43cf5c8
AndreasKuhl <hefbolzen@gmail.com>
2024-09-26 11:32:05 +0200

    .

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
@@ -7,3 +7,25 @@ class S4536 extends StatelessWidget {
     throw UnimplementedError();
   }
 }
+
+class Product {
+  final String name;
+  final double price;
+  Product(this.name, this.price);
+}
+
+class ProductList extends StatelessWidget {
+  final List<Product> products = [
+    Product('Apfel', 0.99),
+    Product('Banane', 0.59),
+    Product('Milch', 1.29),
+    Product('Brot', 2.49),
+    Product('Eier', 1.99),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
