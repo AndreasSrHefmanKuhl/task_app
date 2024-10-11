13db82ee4b70b94b3b4f759f20c0a8eb865178bd
AndreasKuhl <hefbolzen@gmail.com>
2024-10-11 16:05:11 +0200

    fin

--- lib/solutions/chapter_5/subchapter_1/lesson_1/s_5_1_1_2/s_5_1_1_2.dart
+++ lib/solutions/chapter_5/subchapter_1/lesson_1/s_5_1_1_2/s_5_1_1_2.dart
@@ -4,6 +4,64 @@ class S5112 extends StatelessWidget {
   const S5112({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            'assets/images/lama.jpg',
+            height: 300,
+            width: 300,
+          ),
+          const SizedBox(height: 20),
+          const Text(
+            'Name',
+            style: Styles.titleTextStyle,
+          ),
+          const Text(
+            'Kurt das Lama',
+            style: Styles.valueTextStyle,
+          ),
+          const SizedBox(height: 10),
+          const Text(
+            'Gewicht',
+            style: Styles.titleTextStyle,
+          ),
+          const Text(
+            '100 kg',
+            style: Styles.valueTextStyle,
+          ),
+          const SizedBox(height: 10),
+          const Text(
+            'Größe',
+            style: Styles.titleTextStyle,
+          ),
+          const Text(
+            '1,86 m',
+            style: Styles.valueTextStyle,
+          ),
+          const SizedBox(height: 10),
+          const Text(
+            'Lieblingsessen',
+            style: Styles.titleTextStyle,
+          ),
+          const Text(
+            'Rosen',
+            style: Styles.valueTextStyle,
+          ),
+        ],
+      ),
+    );
   }
+}
+
+class Styles {
+  static const titleTextStyle = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const valueTextStyle = TextStyle(
+    fontSize: 16,
+  );
 }
